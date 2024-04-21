8b5a547f75bd80c9e41f74b1137054283f9a89e4
bc780 <124471008+bc780@users.noreply.github.com>
2024-04-21 18:32:34 -0400

    basic gui to interact with
    buttons are non functional, listbox doesn't contain excel sheet yet

--- modify.py
+++ modify.py
@@ -1,4 +1,50 @@
 import pandas as pd
+import tkinter as tk
 
 # Convert the excel sheet into a database
 df = pd.read_excel("Lorcana Card Database.xlsx")
+
+#set up main window
+def add_item():
+    print("test")
+
+
+def remove_item():
+    print("test")
+
+
+def look_items():
+    print("test")
+
+# Create main window
+root = tk.Tk()
+root.title("Lorcana Card Database Editor")
+
+# Entry for input
+labelNum = tk.Label(root, text = "Card Number:")
+labelNum.pack(padx=10)
+entryNum = tk.Entry(root, width=5)
+entryNum.pack(padx=3)
+
+labelExp = tk.Label(root, text = "Expansion Number:")
+labelExp.pack(pady=10)
+entryExp = tk.Entry(root, width=5)
+entryExp.pack(pady=10)
+
+# Listbox to display items
+listbox = tk.Listbox(root, width=40)
+listbox.pack()
+
+# Buttons
+add_button = tk.Button(root, text="Add", command=add_item)
+add_button.pack(side=tk.LEFT, padx=5)
+
+remove_button = tk.Button(root, text="Remove", command=remove_item)
+remove_button.pack(side=tk.LEFT, padx=5)
+
+look_button = tk.Button(root, text="Look", command=look_items)
+look_button.pack(side=tk.LEFT, padx=5)
+
+
+#start application
+root.mainloop()
