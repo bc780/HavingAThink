04606ee69b89c153b8a922b4c5240247e7857adf
BRANDON CHAN <bc780@scarletmail.rutgers.edu>
2024-05-04 17:46:24 -0400

    changed to terminal based ui
    setup for terminal based ui

--- modify.py
+++ modify.py
@@ -1,50 +1,30 @@
 import pandas as pd
-import tkinter as tk
 
 # Convert the excel sheet into a database
 df = pd.read_excel("Lorcana Card Database.xlsx")
 
-#set up main window
-def add_item():
-    print("test")
+while(True):
+    print("\n 1. Add cards\n 2. Search cards\n 3. Remove cards\n 4. Quit\n")
+    choice = input("What would you like to do: ")
+    print(choice)
+    if choice == "1":
+        while(True):
+            num = input("Card Number: ")
+            exp = input("Expansion Number: ")
+            print("\n 1. Normal\n 2. Cold Foil\n 3. Holofoil")
+            foil = input("Foil: ")
 
 
-def remove_item():
-    print("test")
+            repeat = input("Input another card (y or n): ")
+            if (repeat != "y"):
+                break
+    elif choice == "2":
+        print("asdf")
+    elif choice == "3":
+        print("affsdf")
+    elif choice == "4":
+        break
+    else:
+        break
 
-
-def look_items():
-    print("test")
-
-# Create main window
-root = tk.Tk()
-root.title("Lorcana Card Database Editor")
-
-# Entry for input
-labelNum = tk.Label(root, text = "Card Number:")
-labelNum.pack(padx=10)
-entryNum = tk.Entry(root, width=5)
-entryNum.pack(padx=3)
-
-labelExp = tk.Label(root, text = "Expansion Number:")
-labelExp.pack(pady=10)
-entryExp = tk.Entry(root, width=5)
-entryExp.pack(pady=10)
-
-# Listbox to display items
-listbox = tk.Listbox(root, width=40)
-listbox.pack()
-
-# Buttons
-add_button = tk.Button(root, text="Add", command=add_item)
-add_button.pack(side=tk.LEFT, padx=5)
-
-remove_button = tk.Button(root, text="Remove", command=remove_item)
-remove_button.pack(side=tk.LEFT, padx=5)
-
-look_button = tk.Button(root, text="Look", command=look_items)
-look_button.pack(side=tk.LEFT, padx=5)
-
-
-#start application
-root.mainloop()
+print("Saving changes.")
